36f05c3daef2cb88b3e20da34b053b2c80676336
Sean OMeara <someara@opscode.com>
2013-10-16 19:00:41 -0400

    starting down chefspec path

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,14 +1,18 @@
 require 'chefspec'
 
 describe 'iptables::default' do
-  let(:chef_run) { ChefSpec::ChefRunner.new.converge(described_recipe) }
-  let(:lib) { double('library', static_inbound: x, dynamic_inbound: x, static_outbound: x, dynamic_outbound: x) } 
+  let(:lib) { double('library', static_inbound: x, dynamic_inbound: x, static_outbound: x, dynamic_outbound: x) }
+  
+  let(:ubuntu_1004_run) { ChefSpec::ChefRunner.new(platform: 'ubuntu', version: '10.04' ).converge(described_recipe) }
+  let(:ubuntu_1204_run) { ChefSpec::ChefRunner.new(platform: 'ubuntu', version: '10.04' ).converge(described_recipe) }
+  let(:centos5_run) { ChefSpec::ChefRunner.new(platform: 'centos', version: '5.9' ).converge(described_recipe) }
+  let(:centos6_run) { ChefSpec::ChefRunner.new(platform: 'centos', version: '6.4' ).converge(described_recipe) }
 
   before do
     IptablesRules.stub(:new).and_return(lib)
   end
 
-  it 'writes the template' do
-    expect(chef_run).to create_file_with_content('/path/to/template', 'content')
+  it 'creates enclosing directory for persistence file' do
+    expect(chef_run).to create_file_with_content('/etc', 'content')
   end
 end
